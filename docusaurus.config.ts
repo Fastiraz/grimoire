1050228c78455cca664c2b9e1ef2d46482e650e6
Fastiraz <92016718+Fastiraz@users.noreply.github.com>
2025-10-05 15:36:51 +0200

    fix: blog link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -86,7 +86,7 @@ const config: Config = {
           position: 'left',
           label: 'Docs',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
+        // {to: '/blog', label: 'Blog', position: 'left'},
         {
           href: 'https://github.com/fastiraz/neko',
           label: 'GitHub',
@@ -122,10 +122,10 @@ const config: Config = {
         {
           title: 'More',
           items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
+            // {
+            //   label: 'Blog',
+            //   to: '/blog',
+            // },
             {
               label: 'GitHub',
               href: 'https://github.com/fastiraz/neko',
