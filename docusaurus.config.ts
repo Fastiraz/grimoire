c133b1e4e5c86a0126f1e76d6ebd3e116d296c41
Fastiraz <92016718+Fastiraz@users.noreply.github.com>
2025-10-05 14:43:47 +0200

    fix: base url
    edit the base url to `grimoire` for github pages.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,8 @@ const config: Config = {
   url: 'https://fastiraz.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  // baseUrl: '/',
+  baseUrl: '/grimoire/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
